d1f2e37432c44063d4dca020be71900b7b72422c
Rafael De Luca <deluca1712@gmail.com>
2021-04-25 13:28:33 -0700

    validando cadastro com nome, email e senha obrigatorios, unicos e no formato correto

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,36 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "nome é obrigatório" do
+    user = User.new(name: nil)
+
+    user.save
+
+    assert user.errors[:name].any?
+  end
+
+  test "email é obrigatorio e com formato de email valido" do
+    user = User.new(email: nil)
+    user.save
+
+    assert user.errors[:email].any?
+
+    user = User.new(email: "inválido")
+    user.save
+
+    assert user.errors[:email].any?
+
+    user = User.new(email: "valido@examplo.com")
+    user.save
+
+    refute user.errors[:email].any?
+  end
+
+  test "email é tem que ser unico" do
+    user = User.create!(name: "Maria", email: "maria@example.com", password: "secret", password_confirmation: "secret")
+
+    other_user = User.create(email: "rafael@example.com")
+
+    assert other_user.errors[:email].any?
+  end
 end
